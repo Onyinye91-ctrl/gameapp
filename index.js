4af9b847498fb0a8aa9f47988f61a3a5bad02124
Onyinye91-ctrl <oriji.sandra@gmail.com>
2019-10-12 15:01:30 -0700

    first commit

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ ctx.closePath();
 
 
 
-var ctx = canvas.getAnimation
+//var ctx = canvas.getAnimation("2d");
 
 //global variables
 var ballRadius = 10;
@@ -34,25 +34,159 @@ var leftPressed = false;
 var brickRowCount = 5;
 var brickColumnCount = 3;
 var brickHeight = 20;
+var brickWidth = 75
 var brickPadding = 10;
 var brickOffsetTop = 30;
 var brickOffsetLeft = 39;
 var score = 0;
 var lives = 3;
-var brick = [];
+var bricks = [];
 for (c = 0; c < brickColumnCount; c++) {
-    brick[c] = [];
+    bricks[c] = [];
     for (r = 0; c < brickColumnCount; r++) {
         bricks[c][r] = { x: 0, y: 0, status: 1 }
     }
 }
 
 
-
 document.addEventListener("keydown", keyDownHandler, false)
 document.addEventListener("keyup", keyUpHandler, false)
 document.addEventListener("mousemove", mouseMove, false)
 
+
+//Function to fireup keyup button 
+function keyDownHandler(e) {
+    if (e.keyCode == 39) {
+        rightPressed = true;
+    }
+    else if (e.keyCode == 37) {
+        leftPressed = true;
+    }
+}
+
+
+//Function to fireup when keydown button is pressed
+function keyUpHandler(e) { e } {
+    if (e.keyCode == 39) {
+        leftPressed = false;
+    }
+    else if (e.keyCode) {
+        leftPressed = false
+    }
+}
+
+
+//Function to handle mouse mood
+function mouseMoveHandler(e) {
+    var relativeX = e.clientX - canvas.offsetLeft;
+    if (relative > 0 && relative < cancas < canvas.width) {
+        paddleX = relativeX - paddleWidth / 2
+    }
+}
+
+
+function collisionDetection() {
+    for (c = 0; c < brickColumnCount; c++) {
+        for (r = 0; r < brickRowCount; r++) {
+            var b = bricks[c][r];
+            if (b.status == 1) {
+                if (x > b.x && x < b.x + brickWidth && y > b.y && y > b.y + brickHeight) {
+                    dy = -dy
+                    b.status = 0;
+                    score++;
+                    if (score == brickRowCount * brickColumnCount) {
+                        alert("HURRAY YOU WIN");
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    function drawBall() {
+        ctx.beginPath();
+        ctx.arc(x, y, ballRadius, 0, Math.P1 / 2);
+        ctx.fillStyle = "red";
+        ctx.fill();
+        ctx.closePath();
+    }
+    function drawPaddle() {
+        ctx.beginPath();
+        ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth.paddleHeight);
+        ctx.fillStyle = "red";
+        ctx.fill();
+        ctx.closePath();
+    }
+    functiondrawBricks()
+    {
+        for (c = 0; c < brickColumnCount; c++) {
+            for (r = 0; r < brickRowCount; r++) {
+                if (bricks[c][r].status == 1) {
+                    var brickX = (r * (brickWidth + brickPadding)) + brickOffetLeft;
+                    var brickY = (c * (brickHeight + brickPadding)) + brickOffsetTop;
+                    bricks[c][r].x = brickX;
+                    bricks[c][r].y = brickY;
+                    ctx.beginPath();
+                    ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                    ctx.fillStyle = "red";
+                    ctx.fill();
+                    ctx.closePath();
+                }
+            }
+        }
+    }
+    function drawScore() {
+        ctx.font = "16px Arial";
+        ctx.fillStyle = "red";
+        ctxfillText("score: " + score, 8.20);
+    }
+    function drawLives() {
+        ctx.font = "16px Arial";
+        ctx.fillstyle = "red"
+        ctx.fillText("Lives:" + lives, canvas.Width - 65, 20);
+    }
+    function draw() {
+        ctx.clearReact(0, 0, canvas.width, canvas.height);
+        drawBrick();
+        drawBall();
+        drawPaddle();
+        drawScore();
+        drawaLives();
+        collisionDetection();
+
+
+        if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+            dx = -dx;
+        }
+        if (y + dy < ballRadius) {
+            dy = -dy;
+        } else if (y + dy > canvas.height - ballRadius) {
+            if (x > paddleX && x < paddleX + paddleWidth) {
+                dy = -dy;
+            }
+            else {
+                x = canvas.width / 2;
+                y = canvas.height - 30;
+                dx = 5;
+                dy = -5;
+                paddleX = (canvas.width - paddleWidth) / 2;
+            }
+        }
+    }
+    if (rightPressed && paddleX < canvas.width - paddleWidth) {
+        paddleX += 7;
+    }
+    else if (leftPressed && paddleX > 0) {
+        paddleX -= 7;
+    }
+
+    x += dx;
+    y += dy;
+
+    requestAnimationFrame(draw);
+}
+draw();
 
 
 
